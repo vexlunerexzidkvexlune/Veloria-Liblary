6e00513daa478b14eca2fd377410f655eefdc56d
vexlunerexzidkvexlune <hubnevora@gmail.com>
2026-09-16 18:39:00 +0700

    Adjust window position in library.lua

--- library.lua
+++ library.lua
@@ -521,7 +521,7 @@ local Templates = {
         Title = "Veloria Hub",
         Footer = "Veloria Hub Premium Support | By Rexz",
 
-        Position = UDim2.new(0.5, -340, 0.5, -280),
+        Position = UDim2.new(0.5, -360, 0.5, -280),
 Size = UDim2.fromOffset(680, 560),
 IconSize = UDim2.fromOffset(34, 34),
 
